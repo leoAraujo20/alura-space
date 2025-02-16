30d451a981429c238c9a9a5de8bf600d5d5b5829
Leônidas Araújo <leonidasaraujo19@gmail.com>
2025-02-15 21:21:46 -0300

    Update galeria app: enhance Fotografia model, improve image view, and update URL routing

--- galeria/views.py
+++ galeria/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from galeria.models import Fotografia
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'galeria/index.html')
+    fotografias = Fotografia.objects.filter(publicado=True).order_by('-data_fotografia')
+    return render(request, 'galeria/index.html', {"cards":fotografias})
 
-def imagem(request):
-    return render(request, 'galeria/imagem.html')
+def imagem(request, foto_id):
+    fotografia = get_object_or_404(Fotografia, pk=foto_id)
+    return render(request, 'galeria/imagem.html', {"fotografia": fotografia})
