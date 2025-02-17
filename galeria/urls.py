3682897e25f9166de40550d59085c38729f89105
Leônidas Araújo <leonidasaraujo19@gmail.com>
2025-02-17 12:56:41 -0300

    add search functionality, improve image handling, and update templates

--- galeria/urls.py
+++ galeria/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('imagem/<int:foto_id>', views.imagem, name='imagem'),
+    path('imagem/<int:foto_id>/', views.imagem, name='imagem'),
+    path('buscar/', views.buscar, name='buscar')
 ]
