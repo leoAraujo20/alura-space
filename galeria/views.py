1585a99d7ae1c9822ce9427b2b028775cbd3f252
Leônidas Araújo <leonidasaraujo19@gmail.com>
2025-02-20 12:31:27 -0300

    Add user association to Fotografia model and implement login checks in views

--- galeria/views.py
+++ galeria/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from galeria.models import Fotografia
-
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
+    if not request.user.is_authenticated:
+        messages.error(request, 'Faça Login primeiro!')
+        return redirect('login')
+
     fotografias = Fotografia.objects.filter(publicado=True).order_by('-data_fotografia')
     return render(request, 'galeria/index.html', {"cards":fotografias})
 
@@ -12,6 +16,10 @@ def imagem(request, foto_id):
     return render(request, 'galeria/imagem.html', {"fotografia": fotografia})
 
 def buscar(request):
+    if not request.user.is_authenticated:
+        messages.error(request, 'Faça Login primeiro!')
+        return redirect('login')
+
     fotografias = Fotografia.objects.filter(publicado=True).order_by('-data_fotografia')
     if 'buscar' in request.GET:
         nome_a_buscar = request.GET['buscar']
