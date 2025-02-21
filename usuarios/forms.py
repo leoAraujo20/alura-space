47a085c01344883bdf27c03519a7917404fe2516
Leônidas Araújo <leonidasaraujo19@gmail.com>
2025-02-21 09:43:05 -0300

    make the CadastroForm validation with clean method and display the errors in templates

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -72,4 +72,25 @@ class CadastroForm(forms.Form):
                 'placeholder': 'Digite sua senha novamente.'
             }
         )
-    )  
+    )
+
+    def clean_nome_cadastro(self):
+        nome = self.cleaned_data.get('nome_cadastro')
+
+        if nome:
+            nome = nome.strip()
+
+            if ' ' in nome:
+                raise forms.ValidationError('Não é permitido espaços no nome de usuário')
+            else:
+                return nome 
+    
+    def clean_senha2(self):
+        senha1 = self.cleaned_data.get('senha1')
+        senha2 = self.cleaned_data.get('senha2')
+
+        if senha1 and senha2:
+            if senha1 != senha2:
+                raise forms.ValidationError('As senhas não são iguais')
+            else:
+                return senha2
